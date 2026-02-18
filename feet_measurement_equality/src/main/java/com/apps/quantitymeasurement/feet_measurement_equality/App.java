9e5dca9afa8063d1ba8fda8caf3de55ad0725fd0
Arpit Gupta <arpitguptafzd123@gmail.com>
2026-02-18 16:33:47 +0530

    [Arpit Gupta] Add . UC1 implemented successfully

---
+++ feet_measurement_equality/src/main/java/com/apps/quantitymeasurement/feet_measurement_equality/App.java
@@ -0,0 +1,13 @@
+package com.apps.quantitymeasurement.feet_measurement_equality;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
